f4957076b504b03d5da0f67527a9c7006b3c760a
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2023-02-22 09:25:24 +0100

    Fixed unnecessary dependencies for livereload

--- ember-scoped-css/src/app-dependency-loader.js
+++ ember-scoped-css/src/app-dependency-loader.js
@@ -1,10 +1,16 @@
 const fsExists = require('./fsExists');
 
 module.exports = async function (source) {
+  if (this.resourcePath.endsWith('.js')) {
+    const hbsExists = await fsExists(this.resourcePath.replace(/\.js/, '.hbs'));
+    if (hbsExists) {
+      return source;
+    }
+  }
+
   const cssPath = this.resourcePath.replace(/(\.js)|(\.hbs)/, '.css');
   const cssExists = await fsExists(cssPath);
 
-  // add dependency for unnecessary files, probably hbs and js of the same file.
   if (cssExists) {
     this.addDependency(cssPath);
   }
