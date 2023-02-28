bd259d3f702795171427138584df6e0f9757a909
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2023-02-28 11:35:03 +0100

    Added component with imported styles - classic-app

---
+++ test-apps/classic-app/app/components/demo-styles.js
@@ -0,0 +1,4 @@
+import './demo-styles.css';
+import Component from '@glimmer/component';
+
+export default class DemoStylesComponent extends Component {}
--- test-apps/classic-app/ember-cli-build.js
+++ test-apps/classic-app/ember-cli-build.js
@@ -20,6 +20,6 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('node_modules/v2-addon/dist/scoped.css');
+  // app.import('node_modules/v2-addon/dist/scoped.css');
   return app.toTree();
 };
