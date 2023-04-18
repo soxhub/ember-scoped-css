13b4091796619e4b02def3c0ea882730024c16fb
Stanislav Dunajcan <stanislav.dunajcan@gmail.com>
2023-04-18 11:42:22 +0200

    Babel plugin for scopedClass helper

--- ember-scoped-css/src/babel-plugin-scoped-class.js
+++ ember-scoped-css/src/babel-plugin-scoped-class.js
@@ -1,9 +1,32 @@
 const template = require('@babel/template').default;
 const replaceScopedClass = require('./replaceScopedClass');
+const nodePath = require('path');
+const renameClass = require('./renameClass');
+const getPostfix = require('./getPostfix');
 
-const scopedClass = () => {
+const scopedClass = (babel) => {
+  let scopedClassName = '';
   return {
     visitor: {
+      ImportDeclaration(path) {
+        if (path.node.source.value === 'ember-scoped-css') {
+          // find import scopedClass
+          let sc = path.node.specifiers.find(
+            (s) => s.imported.name === 'scopedClass'
+          );
+
+          // store scopedClass local name
+          scopedClassName = sc.local.name;
+
+          // remove import scopedClass
+          if (path.node.specifiers.length === 1) {
+            path.remove();
+          } else {
+            path.node.specifiers = path.node.specifiers.filter((s) => s !== sc);
+          }
+        }
+      },
+
       CallExpression(path, state) {
         if (path.node?.callee?.name === 'precompileTemplate') {
           let source = '';
@@ -21,6 +44,31 @@ const scopedClass = () => {
               state.cwd
             );
           }
+        }
+
+        // scopedClass helper
+        if (
+          scopedClassName &&
+          path.node?.callee?.name === scopedClassName &&
+          path.node.arguments.length === 2 &&
+          path.node.arguments[0].type === 'StringLiteral' &&
+          path.node.arguments[1].type === 'StringLiteral'
+        ) {
+          // get class name and css file path from scopedClass helper
+          let [className, relativeCssFilePath] = path.node.arguments.map(
+            (a) => a.value
+          );
+
+          // rename class
+          let cssFilePath = nodePath.resolve(
+            nodePath.dirname(state.filename),
+            relativeCssFilePath
+          );
+          let postfix = getPostfix(cssFilePath);
+          let renamedClass = renameClass(className, postfix);
+
+          // replace scopedClass helper with renamed class
+          path.replaceWith(babel.types.stringLiteral(renamedClass));
         }
       },
     },
