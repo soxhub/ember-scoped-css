695f7ac39149ce89a877c89dec65e9f49435a5e8
Stanislav Dunajcan <stanislav.dunajcan@gmail.com>
2023-05-24 11:31:24 +0200

    Fixed linting error empty backing class

--- test-apps/classic-app/app/components/header.js
+++ test-apps/classic-app/app/components/header.js
@@ -1,3 +1,7 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default class Header extends Component {}
+export default class Header extends Component {
+  @tracked
+  label = 'header';
+}
