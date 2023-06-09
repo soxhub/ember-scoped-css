06854a225ec98e81f7cb2024a31e6e0e6137a9ef
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-09 16:15:36 -0400

    Round of lint:fix

--- test-apps/classic-app/tests/integration/components/scoped-test.js
+++ test-apps/classic-app/tests/integration/components/scoped-test.js
@@ -1,14 +1,17 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'classic-app/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from 'qunit';
+
+import { setupRenderingTest } from 'classic-app/tests/helpers';
 
 module('Integration | Component | scoped', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it replaces the scoped-class helper', async function (assert) {
     await render(hbs`<Scoped />`);
+
     const elem = this.element.querySelector('div');
+
     assert.deepEqual(
       elem.getAttribute('data-scoped-class'),
       'some-class_e70cde134'
--- test-apps/v2-addon/.eslintrc.cjs
+++ test-apps/v2-addon/.eslintrc.cjs
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const { configs } = require('@nullvoxpopuli/eslint-configs');
+const { configs } = require("@nullvoxpopuli/eslint-configs");
 
 const ember = configs.ember();
 
