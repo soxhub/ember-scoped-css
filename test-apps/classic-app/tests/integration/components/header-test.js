1a6190028810523a283739ec352fb6fb172bca7d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-09 10:15:01 -0400

    Reproduce issue in classic app

--- test-apps/classic-app/tests/integration/components/header-test.js
+++ test-apps/classic-app/tests/integration/components/header-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'classic-app/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { scopedClass } from 'ember-scoped-css/test-helper';
 
 module('Integration | Component | header', function (hooks) {
   setupRenderingTest(hooks);
@@ -11,6 +12,9 @@ module('Integration | Component | header', function (hooks) {
 
     const elem = this.element.querySelector('h1');
     assert.ok(elem.classList.contains('test-header_ee0100766'));
+    assert
+      .dom('h1')
+      .hasClass(scopedClass('test-header', 'app/components/header'));
 
     const style = window.getComputedStyle(elem);
     assert.strictEqual(style.color, 'rgb(255, 0, 0)');
