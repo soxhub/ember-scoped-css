2dcfe0dc48e312548dce511e4090f71095da9968
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-09 16:16:13 -0400

    Fix lint issue in test where gjs did not declare import

--- test-apps/v2-addon/fixtures/expected-dist/components/footer.js
+++ test-apps/v2-addon/fixtures/expected-dist/components/footer.js
@@ -2,6 +2,7 @@ import './footer.css';
 import templateOnly from '@ember/component/template-only';
 import { setComponentTemplate } from '@ember/component';
 import { precompileTemplate } from '@ember/template-compilation';
+import { concat } from '@ember/helper';
 
 var footer = setComponentTemplate(precompileTemplate(`
   <div class='alert_ec12a09e5 ec12a09e5' data-test3={{concat "test" "   my-class_ec12a09e5 my-other-class_ec12a09e5  "}} data-test2="  my-class_ec12a09e5 " data-test="my-class_ec12a09e5">
--- test-apps/v2-addon/src/components/footer.gjs
+++ test-apps/v2-addon/src/components/footer.gjs
@@ -1,3 +1,5 @@
+import { concat } from '@ember/helper';
+
 <template>
   <div class='alert' data-test3={{concat "test" (scoped-class '   my-class my-other-class  ')}} data-test2={{scoped-class '  my-class '}} data-test={{(scoped-class 'my-class')}}>
     <div>
