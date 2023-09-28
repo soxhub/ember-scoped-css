4a4ac3b2d0820ae4fbad5855f33a035328982d89
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-28 11:05:36 -0400

    Fixture needs updating

--- test-apps/v2-addon/fixtures/expected-dist/components/alert.js
+++ test-apps/v2-addon/fixtures/expected-dist/components/alert.js
@@ -1,7 +1,7 @@
 import './alert.css';
-import { setComponentTemplate } from '@ember/component';
-import { precompileTemplate } from '@ember/template-compilation';
 import templateOnly from '@ember/component/template-only';
+import { precompileTemplate } from '@ember/template-compilation';
+import { setComponentTemplate } from '@ember/component';
 
 var TEMPLATE = precompileTemplate("<div class=\"e4b9579df\">\n  <h3 class=\"header_e4b9579df\">\n    {{@title}}\n  </h3>\n  <p class=\"message_e4b9579df {{@some}}\">\n    {{@message}}\n  </p>\n</div>");
 
