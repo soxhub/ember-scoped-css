ebc6f0285f8b7c47662e3ac4886709715845ddfb
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-09-29 19:32:41 -0400

    Revert changes for embroider

--- test-apps/embroider-app/ember-cli-build.js
+++ test-apps/embroider-app/ember-cli-build.js
@@ -9,11 +9,6 @@ module.exports = async function (defaults) {
     // autoImport: {
     //   watchDependencies: ['v2-addon'],
     // },
-    babel: {
-     plugins: [
-        require.resolve('ember-scoped-css/babel-plugin'),
-     ]
-    }
   });
 
   const { Webpack } = require('@embroider/webpack');
@@ -44,17 +39,17 @@ module.exports = async function (defaults) {
                 },
               ],
             },
-            // {
-            //   test: /(\.hbs)|(\.js)$/,
-            //   use: [
-            //     {
-            //       loader: require.resolve(
-            //         'ember-scoped-css/build/app-dependency-loader',
-            //       ),
-            //     },
-            //   ],
-            //   exclude: [/node_modules/, /dist/, /assets/],
-            // },
+            {
+              test: /(\.hbs)|(\.js)$/,
+              use: [
+                {
+                  loader: require.resolve(
+                    'ember-scoped-css/build/app-dependency-loader',
+                  ),
+                },
+              ],
+              exclude: [/node_modules/, /dist/, /assets/],
+            },
           ],
         },
       },
