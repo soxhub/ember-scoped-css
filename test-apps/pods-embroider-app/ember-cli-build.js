e00c27ecd8364d0ffabe183c5ce4c1c597c73567
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-10-04 11:53:21 -0400

    Add new test app for pods

---
+++ test-apps/pods-embroider-app/ember-cli-build.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = async function (defaults) {
+  const { appJsUnplugin } = await import('ember-scoped-css/build');
+
+  const app = new EmberApp(defaults, {
+    // autoImport: {
+    //   watchDependencies: ['v2-addon'],
+    // },
+  });
+
+  const { Webpack } = require('@embroider/webpack');
+
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+    packagerOptions: {
+      // css loaders for live reloading css
+      webpackConfig: {
+        plugins: [appJsUnplugin.webpack({ appDir: __dirname })],
+        module: {
+          rules: [
+            // css loaders for production
+            {
+              test: /\.css$/,
+              use: [
+                {
+                  loader: require.resolve(
+                    'ember-scoped-css/build/app-css-loader',
+                  ),
+                  options: {
+                    layerName: 'embroider-app',
+                  },
+                },
+              ],
+            },
+            {
+              test: /(\.hbs)|(\.js)$/,
+              use: [
+                {
+                  loader: require.resolve(
+                    'ember-scoped-css/build/app-dependency-loader',
+                  ),
+                },
+              ],
+              exclude: [/node_modules/, /dist/, /assets/],
+            },
+          ],
+        },
+      },
+    },
+  });
+};
