e73390fd62d95af2384ac56a03a4b4fc36818139
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-08 16:21:24 -0500

    Add test and further upgrade to demonstrate support for modern syntax

---
+++ test-apps/classic-app/tests/integration/components/v2-addon-ts-with-a-class-test.js
@@ -0,0 +1,21 @@
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from 'qunit';
+
+import { setupRenderingTest } from 'classic-app/tests/helpers';
+
+import { scopedClass } from 'ember-scoped-css/test-support';
+
+module('Integration | Component | WithAClass from v2-addon-ts', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it has scoped class', async function (assert) {
+    await render(hbs`<WithAClass />`);
+
+    assert.dom('div').hasClass('greeting_efc49be66');
+    assert.dom('div').hasStyle({ fontStyle: 'italic' });
+    assert
+      .dom('div')
+      .hasClass(scopedClass('greeting', 'v2-addon-ts/components/with-a-class'));
+  });
+});
