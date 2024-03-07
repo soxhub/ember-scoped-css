e29e58efe935c0a8477bc6fdec530f2f2cd0f7d7
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-07 12:51:43 -0500

    Update lint ignores and lint:fix

--- ember-scoped-css/src/runtime/index.ts
+++ ember-scoped-css/src/runtime/index.ts
@@ -1,9 +1,11 @@
 /**
-* Appends a suffix to a class name during build time.
-* This function is not available at runtime and is removed.
-*
-* @param {string} className the class, defined within the co-located CSS to have a suffix appended to it during build time.
-*/
+ * Appends a suffix to a class name during build time.
+ * This function is not available at runtime and is removed.
+ *
+ * @param {string} className the class, defined within the co-located CSS to have a suffix appended to it during build time.
+ */
 export function scopedClass(className: string): string {
-  throw new Error(`scopedClass is not available at runtime. Cannot do anything with ${className}`);
+  throw new Error(
+    `scopedClass is not available at runtime. Cannot do anything with ${className}`,
+  );
 }
--- test-apps/v2-addon-ts/src/components/strict.gts
+++ test-apps/v2-addon-ts/src/components/strict.gts
@@ -2,6 +2,6 @@ import { scopedClass } from 'ember-scoped-css';
 
 import type { TOC } from '@ember/component/template-only';
 
-export const Strict: TOC<{}> = <template>
+export const Strict: TOC<{ Element: null }> = <template>
   <p class={{scopedClass "hi"}}>text color</p>
 </template>
