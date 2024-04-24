43d480a002a1c8ead1a2a0186d600e979ced96a7
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-04-24 11:02:45 -0400

    Remove app scopedCss webpack becaues it was unused

--- ember-scoped-css/src/build/index.js
+++ ember-scoped-css/src/build/index.js
@@ -1,9 +1,7 @@
 import appJsUnplugin from './app-js-unplugin.js';
-import appScopedcssWebpack from './app-scopedcss-webpack.js';
 import scopedCssUnplugin from './scoped-css-unplugin.js';
 
 export {
   appJsUnplugin,
-  appScopedcssWebpack,
   scopedCssUnplugin,
 };
