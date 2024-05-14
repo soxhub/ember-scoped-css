8a85a432100f8e7b4c216808a264559e5fbb5dde
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-05-14 18:22:36 -0400

    have to adjust lints to account for newly-linted files

--- test-apps/v2-addon/.eslintrc.cjs
+++ test-apps/v2-addon/.eslintrc.cjs
@@ -6,5 +6,15 @@ const ember = configs.ember();
 
 // accommodates: JS, TS, App, Addon, and V2 Addon
 module.exports = {
-  overrides: [...ember.overrides],
+  overrides: [
+    ...ember.overrides,
+    {
+      files: ['**/*.gjs', '**/*.gts'],
+      rules: {
+        // scoped-class isn't defined, yet we allow it for build time.
+        // scopedClass is importable, and removed at build time as well.
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
