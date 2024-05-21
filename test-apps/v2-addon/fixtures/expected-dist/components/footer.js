eb5e1dbb9bb01165e97f3a0347f3e3d2eef66a9f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-05-21 13:16:44 -0400

    Update fixture

--- test-apps/v2-addon/fixtures/expected-dist/components/footer.js
+++ test-apps/v2-addon/fixtures/expected-dist/components/footer.js
@@ -5,10 +5,10 @@ import { setComponentTemplate } from '@ember/component';
 import templateOnly from '@ember/component/template-only';
 
 var footer = setComponentTemplate(precompileTemplate("\n  <div class=\"alert_e9a64d6f1 e9a64d6f1\" data-test3={{concat \"test\" \"   my-class_e9a64d6f1 my-other-class_e9a64d6f1  \"}} data-test2=\"  my-class_e9a64d6f1 \" data-test=\"my-class_e9a64d6f1\">\n    <div class=\"e9a64d6f1\">\n      {{@title}}\n    </div>\n    <p class=\"e9a64d6f1\">\n      {{@message}}\n    </p>\n  </div>\n", {
-  strictMode: true,
   scope: () => ({
     concat
-  })
+  }),
+  strictMode: true
 }), templateOnly());
 
 export { footer as default };
