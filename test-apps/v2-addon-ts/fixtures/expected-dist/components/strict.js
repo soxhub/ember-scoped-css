9072502659edf4ebb40697e4bd9644154afb7061
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-05-21 12:16:38 -0400

    Revert "Fix fixture tests"
    This reverts commit a198c084bae82a91d9185a30ce0864cb3b546d37.

--- test-apps/v2-addon-ts/fixtures/expected-dist/components/strict.js
+++ test-apps/v2-addon-ts/fixtures/expected-dist/components/strict.js
@@ -6,11 +6,11 @@ import templateOnly from '@ember/component/template-only';
 
 const noop = () => {};
 const Strict = setComponentTemplate(precompileTemplate("\n  <p class=\"hi_e0d942f65\">text color</p>\n  {{!-- this button is part of the test to ensure we don't blow away this scope bag--}}\n  <button type=\"button\" {{on \"click\" noop}}>e</button>\n", {
-  strictMode: true,
   scope: () => ({
     on,
     noop
-  })
+  }),
+  strictMode: true
 }), templateOnly());
 
 export { Strict };
