58153007747ae320d2d195159df196933b3a707d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-04-08 17:52:32 -0400

    Drop 4.4

--- test-apps/embroider-app/config/ember-try.js
+++ test-apps/embroider-app/config/ember-try.js
@@ -40,6 +40,14 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-5.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.12.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
