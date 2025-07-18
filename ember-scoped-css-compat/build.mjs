1189fb6b043ccb429abaf192ed1193450d9af50e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-18 12:27:23 -0400

    Build

---
+++ ember-scoped-css-compat/build.mjs
@@ -0,0 +1,33 @@
+import * as esbuild from "esbuild";
+import { vitestCleaner } from "esbuild-plugin-vitest-cleaner";
+import { createRequire as topLevelCreateRequire } from "module";
+
+const require = topLevelCreateRequire(import.meta.url);
+
+const buildFiles = ["node/ember-classic-support.mjs"];
+
+const external = [...Object.keys(require("./package.json").dependencies)];
+
+// Node, CJS
+await esbuild.build({
+  entryPoints: buildFiles,
+  bundle: true,
+  outdir: "dist/cjs",
+  format: "cjs",
+  platform: "node",
+  sourcemap: "inline",
+  outExtension: { ".js": ".cjs" },
+  external,
+  plugins: [vitestCleaner()],
+});
+
+// Runtime
+await esbuild.build({
+  entryPoints: ["src/runtime/test-support.ts", "src/runtime/index.ts"],
+  sourcemap: true,
+  format: "esm",
+  bundle: true,
+  external,
+  plugins: [vitestCleaner()],
+  outdir: "dist/runtime",
+});
